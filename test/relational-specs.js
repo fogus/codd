2326b594d1eef08047252a8a9d669d5d93be52ec
fogus <mefogus@gmail.com>
2013-02-15 11:17:21 -0500

    Rename L.$.RQL funs

--- test/relational-specs.js
+++ test/relational-specs.js
@@ -21,10 +21,10 @@ describe("Relational algebra functions", function() {
     });
   });
 
-  describe("L.$.select", function() {
+  describe("L.$.restrict", function() {
     it("should return an array of uniq elements matching a predicate", function() {
       var s = L.$(1,2,3,4,5,3,4,5);
-      var result = L.$.select(s, L.isOdd);
+      var result = L.$.restrict(s, L.isOdd);
 
       expect(result.constructor).toBe(Array);
       expect(result).toEqual([1,3,5]);
