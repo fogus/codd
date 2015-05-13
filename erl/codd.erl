5d9b42d2ae141b1cd20204e17cb6bf821e08e599
fogus <mefogus@gmail.com>
2015-05-13 16:08:26 -0400

    is_truthy

--- erl/codd.erl
+++ erl/codd.erl
@@ -1,7 +1,12 @@
 -module(codd).
--export([test/2]).
+-export([test/2, is_truthy/1]).
 -import(maps, [new/0]).
 -import(sets, [from_list/1]).
+
+% Courtesy of Juan Facorro
+% 
+is_truthy(X) ->
+     X =/= false andalso undefined =/= X.
 
 test(Name1, Name2) ->
     sets:from_list([#{"person" => Name1}, #{"person" => Name2}]).
